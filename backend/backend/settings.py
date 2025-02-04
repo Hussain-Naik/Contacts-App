ee06e500677f8bd7665bd4666153ff42490de324
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2025-02-04 20:53:10 +0000

    Add frontend staticfiles directory to backend settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,6 +136,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR.parent, "frontend/build/static"),
+    os.path.join(BASE_DIR.parent, "frontend/build"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR.parent, "static")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

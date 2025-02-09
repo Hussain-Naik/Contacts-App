d3b4ae36408010286be8de98c881155abd675ac8
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2025-02-09 13:03:31 +0000

    Add page not found component

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <p>Not found</p>
-    </div>
-  )
-}
+    <ListGroup>
+      <ListGroup.Item
+      className="justify-content-center"
+        onClick={() => navigate(`/`)}
+        action
+        variant="warning"
+      >
+        <h2>Page not found!</h2>
+        <p>We could not find what you were looking for. Please click here to be redirected to home</p>
+      </ListGroup.Item>
+    </ListGroup>
+  );
+};
 
-export default NotFound
+export default NotFound;

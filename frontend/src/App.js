e0c50c337f74f31940f529cc02e55d6080f12f08
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2025-02-09 09:26:46 +0000

    Add body css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import styles from "./App.module.css";
-import { Route, Routes } from 'react-router-dom/';
+import { Route, Routes } from "react-router-dom/";
 // import Footer from './components/Footer';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Contacts from "./pages/Contacts";
 import CreateContact from "./pages/CreateContact";
 import Header from "./components/Header";
@@ -13,27 +13,22 @@ import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <>
-    <div className={styles.App}>
-      <Container data-bs-theme='dark'>
-        <Header />
-        <main>
-          <Row className="justify-content-md-center">
-            <Col sm={12} md={6}>
-              <Routes>
-                  <Route exact path="/" element={<Contacts />} />
-                  <Route exact path="/create/" element={<CreateContact />} />
-                  <Route exact path="/contact/:id" element={<ContactDetail />} />
-                  <Route exact path="/edit/:id" element={<EditContact />} />
-                  <Route path="*" element={<NotFound />} />
-              </Routes>
-            </Col>
-          </Row>
-        </main>
-      </Container>
-      {/* <Footer /> */}
-    </div>
-    </>
+    <Container className={styles.App} data-bs-theme="dark">
+      <Header />
+      <main>
+        <Row className="justify-content-md-center">
+          <Col sm={12} md={6}>
+            <Routes>
+              <Route exact path="/" element={<Contacts />} />
+              <Route exact path="/create/" element={<CreateContact />} />
+              <Route exact path="/contact/:id" element={<ContactDetail />} />
+              <Route exact path="/edit/:id" element={<EditContact />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Col>
+        </Row>
+      </main>
+    </Container>
   );
 }
 

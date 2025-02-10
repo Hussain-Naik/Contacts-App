329dee7e4c17b868eac9649c26c89356d89c039e
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2025-02-10 21:02:07 +0000

    Add allowed hosts urls

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,7 +46,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
